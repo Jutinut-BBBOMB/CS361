56cb252299765a95fd0fdfefce145cec906b5aee
PHURIT FEUNGFU - 6609650038 <145310722+PHURITfuengfu@users.noreply.github.com>
2025-11-16 20:39:37 +0700

    Update report-lost-item-function.py

--- lambdafunction/report-lost-item-function.py
+++ lambdafunction/report-lost-item-function.py
@@ -7,7 +7,7 @@ import datetime
 import secrets
 
 DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME', 'Items_TU')
-S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'your-bucket-name')
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'adminwebtulostfound')
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 s3 = boto3.client('s3', region_name='us-east-1')
@@ -124,3 +124,4 @@ def lambda_handler(event, context):
             'headers': CORS_HEADERS,
             'body': json.dumps({'error': 'Failed to save data'})
         }
+
